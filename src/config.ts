a3f5fe4147fe22c5b97a08517119a09ac34ff8a0
Sven Wasmer <sven.wasmer@gmail.com>
2025-08-30 19:21:27 +0100

    remove umami id

--- src/config.ts
+++ src/config.ts
@@ -139,7 +139,7 @@ export const themeConfig: ThemeConfig = {
     googleAnalyticsID: '',
     // umami analytics
     // https://cloud.umami.is
-    umamiAnalyticsID: '520af332-bfb7--9386-5f273ee3d697',
+    umamiAnalyticsID: '',
     // follow verification
     // https://follow.is/
     follow: {
